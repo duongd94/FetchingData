76b62b3d148c82a4ac36fc3a1491ed62a624a0cb
duongd94 <31262195+duongd94@users.noreply.github.com>
2019-04-19 16:51:42 -0700

    Add files via upload

--- comments.js
+++ comments.js
@@ -39,27 +39,35 @@
 	// .catch(fuction(error){
 		// console.error('network error:/n, error);
 	// });
-  const DATA_SELECTOR = '[data-keys="id"]';
-  var jData = document.querySelector(DATA_SELECTOR);
 
-	let postnumber = 0;
-	for(var i = 0; i < 100; i++)
-	{
-		fetch('https://jsonplaceholder.typicode.com/post/' + postnumber)
-			.then(response => response.json())
-			.then(json => console.log(jData))
-      .catch(error => { console.error('Error Fetching')
-    });
-		for (var key in jData){
-			if (json.hasOwnProperty(key)) {
-				alert(json[key].userID);
-				alert(json[key].id);
-				alert(json[key].title);
-				alert(json[key].body);
-			};
-		};
-		postnumber++;
-	};
+
+    const DATA_SELECTOR = '[data-keys="id"]';
+    let jData = document.querySelector(DATA_SELECTOR);
+
+    jData.forEach(function (data) {
+
+    let sectionSelector = `#posts-${data.value}`;
+    let commentSection = document.querySelector(sectionSelector);
+
+  	let postnumber = 0;
+  	for(var i = 0; i < 100; i++)
+  	{
+  		fetch('https://jsonplaceholder.typicode.com/post/' + postnumber)
+  			.then(response => response.json())
+  			.then(json => console.log(jData))
+        .catch(error => { console.error('Error Fetching')
+      });
+  		for (var key in jData){
+  			if (json.hasOwnProperty(key)) {
+  				alert(json[key].userID);
+  				alert(json[key].id);
+  				alert(json[key].title);
+  				alert(json[key].body);
+  			};
+  		};
+  		postnumber++;
+	   };
+   });
 
 
 
